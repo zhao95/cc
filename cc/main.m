0054236c7b0d2cf384a0072376009f62f81c7409
泽熙 <zhao95@outlook.com>
2016-05-14 10:07:11 +0800

    Initial Commit

---
+++ cc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cc
+//
+//  Created by 赵恩峰 on 16/5/14.
+//  Copyright © 2016年 赵恩峰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
